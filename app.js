829c299096c74e2c3c1faa58760eb385aa3ac5fd
Himansh <himanshbansal200521@gmail.com>
2024-03-16 11:08:19 +0530

    Setting up initials

---
+++ app.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = 5002 || process.env.PORT;
+
+app.get('',(req,res)=>{
+    res.send("Hello World");
+});
+
+app.listen(PORT,()=>{
+    console.log(`app listening on ${PORT}`);
+});
